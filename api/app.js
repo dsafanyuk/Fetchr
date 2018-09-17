16623796309c3ee24d4d8df4e8c46e473d7f7094
Dave Safanyuk <dsafanyuk@gmail.com>
2018-09-17 11:16:23 -0500

    refactored file structure

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes');
 var app = express();
 app.use(logger('dev'));
 app.use(express.json());
