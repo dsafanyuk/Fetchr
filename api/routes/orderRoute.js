1193bb9063b7878cf08bd02157755494dd598ddb
Dave Safanyuk <dsafanyuk@gmail.com>
2018-09-17 21:38:46 -0500

    Created skeleton for the api

---
+++ api/routes/orderRoute.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+orderController = require('../controllers/orders/orderController')
+
+router.get('/order', orderController.showAllOrders)
+router.get('/order/:id', orderController.showOneOrder)
+router.patch('/order/:id', orderController.updateOrder)
+router.post('/order', orderController.createOrder)
+router.delete('/order/:id', orderController.deleteOrder)
+
+module.exports = router;
