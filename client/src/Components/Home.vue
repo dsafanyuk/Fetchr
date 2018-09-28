2370037ca12f6418aed5eb01c6a271f425b08344
Drew Twelse <kesney095@gmail.com>
2018-09-27 19:58:39 -0700

    Login and Registration Commponents added

---
+++ client/src/Components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div class="alert alert-primary" role="alert">
+      A simple primary alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import 'css/home.css'
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './Components/Home.vue'
+import VueRouter from 'vue-router'
+import Login from './Components/Login.vue'
+import Register from './Components/Register.vue'
+
+Vue.use(VueRouter);
+
+/*----------------------- Routes Declaration -----------------*/
+const routes = [
+  {path : '/login', component : Login},
+  {path : '/register', component : Register}
+];
+
+const router = new VueRouter({
+  routes,
+  mode : 'history'
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
