db0e2ff4fa557da1f5c36893b1351b3c36db55ce
Dave Safanyuk <dsafanyuk@gmail.com>
2018-09-28 21:18:10 -0500

    Created most user routes with error handling

--- api/users/userController.js
+++ api/users/userController.js
@@ -9,8 +9,10 @@ function showAllUsers(req, res) {
         .then((rows) => {
             res.send(rows).status(200)
         })
-        .catch(function (e) {
-            res.send(e.sqlMessage)
+        .catch(function (err) {
+            res.status(500).send({
+                message: `${err}`
+            }) // FOR DEBUGGING ONLY, dont send exact message in prod
         })
 }
 
@@ -20,8 +22,22 @@ function showOneUser(req, res) {
         .then((rows) => {
             res.send(rows).status(200)
         })
-        .catch(function(err){
-            res.status(500).send({ message: `${err}`}) // FOR DEBUGGING ONLY, send 500 only in prod
+        .catch(function (err) {
+            res.status(500).send({
+                message: `${err}`
+            }) // FOR DEBUGGING ONLY, dont send exact message in prod
+        })
+}
+// GET /user/{id}/orders
+function showUserOrders(req, res) {
+    knex("orders").select('*').where('customer_id', req.params.user_id)
+        .then((rows) => {
+            res.send(rows)
+        })
+        .catch(function (err) {
+            res.status(500).send({
+                message: `${err}`
+            }) // FOR DEBUGGING ONLY, dont send exact message in prod
         })
 }
 
@@ -29,11 +45,19 @@ function showOneUser(req, res) {
 function createUser(req, res) {
     let request = req.body;
     knex("users").insert(request)
-        .then(function () {
-            res.status(201).send('user created')
+        // if user successfully inserted
+        .then((rows) => {
+            // Select the user that was just created
+            knex("users").select('*').where('user_id', rows)
+                .then((rows) => {
+                    res.status(201).send(`User created: ${rows[0].email_address}`)
+                })
         })
-        .catch(function (e) {
-            res.send(e.sqlMessage)
+        // else send err
+        .catch(function (err) {
+            res.status(500).send({
+                message: `${err}`
+            }) // FOR DEBUGGING ONLY, dont send exact message in prod
         })
 }
 
@@ -44,13 +68,30 @@ function updateUser(req, res) {
 }
 
 // DELETE /user/{id}
+//TODO primary key error
+//********************* */
+/* Unhandled rejection Error: ER_ROW_IS_REFERENCED_2:
+ Cannot delete or update a parent row: a foreign key constraint fails 
+ (`fetchr_db`.`orders`, CONSTRAINT `courier_fk` 
+ FOREIGN KEY (`courier_id`) 
+ REFERENCES `users` (`user_id`))
+*/
 function deleteUser(req, res) {
-    res.send("delete me");
+    knex("users").where('user_id', req.params.user_id).del()
+        .then((rows) => {
+            res.send('success').status(202)
+        })
+        .catch(function (err) {
+            res.status(500).send({
+                message: `${err}`
+            }) // FOR DEBUGGING ONLY, dont send exact message in prod
+        })
 }
 
 module.exports = {
     showAllUsers: showAllUsers,
     showOneUser: showOneUser,
+    showUserOrders: showUserOrders,
     createUser: createUser,
     updateUser: updateUser,
     deleteUser: deleteUser
