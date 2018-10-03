933e2314fa41bfc472cc456622f3ad56b3c5b21a
James Limantara <james.limantara@students.pcci.edu>
2018-10-02 20:25:11 -0500

    edited order routes

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ module.exports =  options = {
     connection: {
         host: '127.0.0.1',
         user: 'root',
-        password: 'password',
+        password: 'helloworld',
         database: 'fetchr_db'
     }
 }
--- api/orders/orderController.js
+++ api/orders/orderController.js
@@ -1,19 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var dbOptions = require("../db");
+
+const knex = require("knex")(dbOptions);
 
 // GET /order
 function showAllOrders(req,res){
-    res.send('show all orders')
+    knex('orders').select('*')
+        .then((rows) => {
+            res.send(rows).status(200)
+        })
+        .catch(function (err) {
+            res.status(500).send({
+                message: `${err}`
+            }) // FOR DEBUGGING ONLY, dont send exact message in prod
+        })
 }
 
 // GET /order/{id}
 function showOneOrder(req,res){
-    res.send('show one order')
+    knex('orders').where('order_id', req.params.order_id)
+        .then((rows) => {
+            res.send(rows).status(200)
+        })
+        .catch(function (err) {
+            res.status(500).send({
+                message: `${err}`
+            }) // FOR DEBUGGING ONLY, dont send exact message in prod
+        })
 }
 
 // POST /order
 function createOrder(req,res){
-    res.send('create order')
+    let request = req.body;
+    knex("orders").insert(request)
+        // if order successfully inserted
+        .then((user_id) => {
+            // Select the order that was just created
+            console.log(rows)
+            knex("orders").select('*').where('order_id', order_id)
+                .then((rows) => {
+                    res.status(201).send(`Order created for id ${rows[0].order_id}`)
+                })
+        })
+        // else send err
+        .catch(function (err) {
+            res.status(500).send({
+                message: `${err}`
+            }) // FOR DEBUGGING ONLY, dont send exact message in prod
+        })
 }
 
 // UPDATE /order{id}
@@ -23,7 +58,15 @@ function updateOrder(req,res){
 
 // DELETE /order/{id}
 function deleteOrder(req,res){
-    res.send('delete me')
+    knex("orders").where('order_id', req.params.order_id).del()
+        .then((rows) => {
+            res.send('success').status(202)
+        })
+        .catch(function (err) {
+            res.status(500).send({
+                message: `${err}`
+            }) // FOR DEBUGGING ONLY, dont send exact message in prod
+        })
 }
 
 
