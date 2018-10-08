41ec10e95820aa501ab8ece746388d9f32c97837
James Limantara <james.limantara@students.pcci.edu>
2018-10-07 19:55:18 -0500

    yeeeee boiiiiiiiiiii

--- api/app.js
+++ api/app.js
@@ -1,16 +1,31 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var dbOptions = require('./db')
+var   express       = require('express');
+var   path          = require('path');
+var   cookieParser  = require('cookie-parser');
+var   bodyParser    = require('body-parser')
+var   logger        = require('morgan');
+var   dbOptions     = require('./db')
 const knex = require('knex')(dbOptions);
-var apiRouter = require('./routes/index');
-var app = express();
+var   passport      = require('passport');
+var   LocalStrategy = require('passport-local').Strategy;
+var   session       = require('express-session')
+var   apiRouter     = require('./routes/index');
+var   app           = express();
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+/*app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}))*/
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('*', (req,res,next)=>{
     console.log('middle ware 1')
--- api/users/userRoute.js
+++ api/users/userRoute.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router({mergeParams: true}) // don't forget the parent params!
+var   passport      = require('passport');
+var   LocalStrategy = require('passport-local').Strategy;
 
 userController = require('./userController')
+
+router.post('/register', userController.registerUser)
+
+router.post('/login', passport.authenticate('local', {successRedirect: '/', failureRedirect:'/login', failureFlash: true}), userController.loginUser)
 
 router.get('/', userController.showAllUsers)
 
 router.get('/:user_id/',userController.showOneUser)
 
 router.get('/:user_id/orders', userController.showUserOrders)
-
-router.get('/register', userController.registerUser)
 
 router.patch('/:user_id', userController.updateUser)
 
