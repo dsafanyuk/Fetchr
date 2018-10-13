88d06c88b000cbae4508bebdae38069aa73bc445
Dave Safanyuk <dsafanyuk@gmail.com>
2018-10-12 21:18:40 -0500

    Added cors to server + added axios to vue/package.json

--- api/app.js
+++ api/app.js
@@ -5,12 +5,17 @@ var   logger        = require('morgan');
 var   apiRouter     = require('./routes/index');
 var   auth          = require('./auth')();
 var   app           = express();
+var   cors          = require('cors')
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Enable cross origin requests 
+// This allows Vue to make requests to the server
+app.use(cors())
 
 app.use(auth.initialize());
 
