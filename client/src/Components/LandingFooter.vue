1e34f611b7389abe26517d668de2060f72332785
Drew Twelse <kesney095@gmail.com>
2018-10-18 15:36:30 -0700

    Font awesome 4 added & Footer is Done. Still working on the Filter

--- client/src/Components/LandingFooter.vue
+++ client/src/Components/LandingFooter.vue
@@ -1,19 +1,53 @@
-<template >
+<template>
 
-        <footer id="LandingFooter">
-          <h4> </h4>
+  <footer id="LandingFooter" class="footer">
+    <div class="container bottom_border">
+      <div class="row">
 
-        </footer>
+
+        <ul class="foote_bottom_ul_amrc">
+          <li><a href="#">Terms</a></li>
+          <li><a href="#">Privacy</a></li>
+          <li><a href="#">Help</a></li>
+          <li><a href="#">Blog</a></li>
+
+        </ul>
+
+      </div>
+    </div>
+
+
+    <div class="container">
+      <ul class="foote_bottom_ul_amrc">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <!--foote_bottom_ul_amrc ends here-->
+      <p class="text-center">Copyright @2018 | All Right Reserved</p>
+
+      <ul class="social_footer_ul">
+        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+      </ul>
+      <!--social_footer_ul ends here-->
+    </div>
+
+  </footer>
 
 </template>
 
 <script>
-export default {
-  name : "LandingFooter"
+  export default {
+    name: "LandingFooter"
 
   }
 </script>
 
 <style>
-@import 'custom_css/footer.scss'
+  @import 'custom_css/footer.scss'
 </style>
