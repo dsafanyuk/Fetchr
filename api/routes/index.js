1bf3e50d3e9c35c0f05ecbd6a964b06386673dd4
Dave Safanyuk <dsafanyuk@gmail.com>
2018-10-26 15:28:33 -0500

    Initial Product Route (#19)
    * Initial push
    
    * added vscode folder to gitignore
    
    * Added a `/product` route.
    
    All products lists by items sold desc by default
    
    Products by category lists by items sold -> desc by default but also has options to sort by `price`
    eg:
    `api/products/filter/drink?sort[total_sold]=desc`
    * returns all drinks sorted by total sold (descending)
    
    `/api/products/filter/drink?sort[price]=desc`
    * returns all drinks by price (descending)
    
    * Added a TODO validate incoming queries
    
    * Created changes
    
    * Added a new view `product_records` where we can get items by popularity

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var userRouter = require ('../users/userRoute')
-var orderRouter = require ('../orders/orderRoute')
+var userRouter = require('../users/userRoute');
+var orderRouter = require('../orders/orderRoute');
+var productRouter = require('../products/productRoute');
 
-router.use('/users', userRouter)
-router.use('/orders', orderRouter)
+router.use('/users', userRouter);
+router.use('/orders', orderRouter);
+router.use('/products', productRouter);
 
 module.exports = router;
