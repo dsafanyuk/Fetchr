d636669e28b1a90cc1353081a6237ca8e4594490
drewtwelse02 <kesney095@gmail.com>
2018-11-05 13:37:32 -0800

    Landing (#25)
    *  Header& Footer Component created for the Landing.
    
    * Part of the filter done
    
    *  Font awesome 4 added & Footer is Done. Still working on the Filter
    
    * Blog Removed in the Footer & Spelling Error Corrected
    
    * Card Implemented on the Dashboard
    
    *  Vuetifyjs Added
    
    * Product Dialog Created
    
    * Product Info Dialog Implemented
    
    * Popup work
    
    *  Last Update
    
    * Landing Route changed to Dashboard
    
    * Added Vue Cookie. Fixed some design issues
    
    * added the registerRedirect + product listing
    
    * Signed-off-by: Kesney <kesney095@gmail.com>
    
    * Vuetified header + added css fixes
    
    * Last update

---
+++ client/src/Components/mini-components/LandingHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="custom_url col-md-4 navbar-nav  mt-2 ">
+          <li class="nav-item active">
+            <img src="fetchr_logo.png" width="80" height="80" class="d-inline-block align-top"align="left">
+          </li>
+        </ul>
+        <form class="col-md-4 form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2 " size="40" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <div class="col-md-4 icons_div">
+          <i class="far fa-user"></i>
+          <i class="fas fa-shopping-cart"></i>
+        </div>
+      </div>
+    </nav>
+</template>
+<script>
+export default {
+
+  data () {
+    name: "LandingHeader"
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="css">
+@import '../custom_css/landing.scss'
+</style>
---
+++ client/src/Components/mini-components/ShoppingCart.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="col-md-12 shopping-cart">
+      <div class="shopping-cart-header">
+      
+        <div class="shopping-cart-total">
+          <span class="lighter-text">Total:</span>
+          <span class="main-color-text">$2,229.97</span>
+        </div>
+      </div> <!--end shopping-cart-header -->
+
+      <ul class="shopping-cart-items">
+        <li class="clearfix">
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/cart-item1.jpg" alt="item1" />
+          <span class="item-name">Sony DSC-RX100M III</span>
+          <span class="item-price">$849.99</span>
+          <span class="item-quantity">Quantity: 01</span>
+        </li>
+
+        <li class="clearfix">
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/cart-item2.jpg" alt="item1" />
+          <span class="item-name">KS Automatic Mechanic...</span>
+          <span class="item-price">$1,249.99</span>
+          <span class="item-quantity">Quantity: 01</span>
+        </li>
+
+        <li class="clearfix">
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/cart-item3.jpg" alt="item1" />
+          <span class="item-name">Kindle, 6" Glare-Free To...</span>
+          <span class="item-price">$129.99</span>
+          <span class="item-quantity">Quantity: 01</span>
+        </li>
+      </ul>
+
+      <a href="#" class="button">Checkout</a>
+    </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="css">
+@import '../custom_css/ShoppingCart.scss';
+</style>
