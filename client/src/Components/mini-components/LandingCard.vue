6c39bfa7636906749d761bbacd527e74d3059239
Dave Safanyuk <dsafanyuk@gmail.com>
2018-11-18 16:16:45 -0600

    Product card edits (#27)
    * Changed product cards
    
    * updated product images
    
    * fixed the navbar being affected by materialcss + image fixes
    
    * Added visual 'favorite' triggers to product cards
    
    * Added 'add to cart' visual cue, fixed header spacing
    
    *  Fixed header icons
    
    * Shopping cart added
    
    * Revert "Added 'add to cart' visual cue, fixed header spacing"
    
    Removed cart components that were added by accident
    
    This reverts commit 1441875847e7a471d273425f93c9dda4701cd037.
    
    * Added `add to cart` visuals

--- client/src/Components/mini-components/LandingCard.vue
+++ client/src/Components/mini-components/LandingCard.vue
@@ -1,54 +1,84 @@
 <template>
-  <div id="landing_card" class=" card_prop col-md-3 col-xs-2">
-     <div class="thumb">
-          <ProductInfo>
+<div class="col-lg-3 m8 s12 offset-m2">
+    <div class="product-card">
+        <div class="card  z-depth-4">
+            <div class="card-img">
+                <span href="#" class="btn-floating btn-large price waves-effect waves-light brown darken-3">{{product.price}}</span>
+                <img :src=product.product_url :alt=product.product_name>
+            </div>
+                <ul class="card-action-buttons">
+                    <li><a v-on:click="favorite()" id="favorite" class="btn-floating waves-effect waves-light red accent-2">
+                            <i v-if="isFavorite" class="material-icons like">favorite</i>
+                            <i v-if="!isFavorite" class="material-icons like">favorite_border</i>
+                        </a>
+                    </li>
+                    <li><a v-on:click="addToCart()" id="buy" class="btn-floating waves-effect waves-light blue">
+                            <i v-if="inCart" class="material-icons buy">check</i>
+                            <i v-if="!inCart" class="material-icons buy">add_shopping_cart</i>
+                        </a>
+                    </li>
+                </ul>
+                <div class="card-content">
+                    <div class="row">
+                        <div class="col s12">
+                            <p>
+                                <span class="card-title text-center">
+                <span>{{product.product_name}}</span></span> <br />
+                        </p>
+                        </div>
 
-           </ProductInfo>
-     </div>
-     <div class="details">
-        <div class="row">
-  	     <div class=" col-md-8 col-xs-9">
-              <span>{{product.product_name}}</span>
-  		 </div>
-  		 <div class=" col-md-4 col-xs-3 links">
-              <a href="#"><i class="far fa-heart fa_la"></i></a>
-           </div>
-  		 <br><br>
-  	  </div>
-        <div class="bottom-details">
-  	     <div class="price">{{product.price}}</div>
-  	     <div class="links">
-  	        <a href=""><i class="fa fa-shopping-cart fa-md"></i></a>
-  	     </div>
+                    </div>
+                </div>
+            </div>
         </div>
-     </div>
-
-  </div>
+    </div>
 </template>
 
 <script>
-import ProductInfo from './ProductInfo.vue';
+import Toasted from 'vue-toasted';
 
 export default {
   props: {
-  product: {
-    product_id: String,
-    product_name: String,
-    price: Number,
-    category: String,
-    product_url: String,
+    product: {
+      product_id: String,
+      product_name: String,
+      price: Number,
+      category: String,
+      product_url: String,
+    },
   },
-},
   data() {
     return {
+      isFavorite: false,
+      inCart: false,
     };
   },
-  components: {
-    ProductInfo: ProductInfo,
+  components: {},
+  methods: {
+    favorite: function() {
+      if (this.isFavorite) {
+        this.isFavorite = false;
+        this.$toasted.success('Unfavorited').goAway(1000);
+      } else {
+        this.isFavorite = true;
+        this.$toasted.success('Favorited').goAway(1000);
+      }
+    },
+    addToCart: function() {
+      if (this.inCart) {
+        this.inCart = false;
+        this.$toasted.success('Removed from cart').goAway(1000);
+      } else {
+        this.inCart = true;
+        this.$toasted.success('Added to cart').goAway(1000);
+      }
+    },
   },
 };
 </script>
 
-<style lang="css">
+<style scoped lang="css">
 @import '../custom_css/landing_card.scss';
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+@import '../custom_css/materialize.css';
 </style>
--- client/src/Components/mini-components/LandingHeader.vue
+++ client/src/Components/mini-components/LandingHeader.vue
@@ -3,32 +3,98 @@
       <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="custom_url col-md-4 navbar-nav  mt-2 ">
           <li class="nav-item active">
-            <img src="fetchr_logo.png" width="80" height="80" class="d-inline-block align-top"align="left">
+            <img src="https://s3.us-east-2.amazonaws.com/fetchrapp/Logo.png" width="80" height="80" class="d-inline-block align-top"align="left">
           </li>
         </ul>
         <form class="col-md-4 form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2 " size="40" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
         <div class="col-md-4 icons_div">
-          <i class="far fa-user"></i>
-          <i class="fas fa-shopping-cart"></i>
+          <div class="col-md-3">
+          </div>
+          <div class="col-md-4" style="display: -webkit-inline-box;">
+          <i class="far fa-user fa-xs"></i>
+       <li class="dropdown">
+         <a href="#" class="" data-toggle="dropdown" role="button" aria-expanded="false"> <i class="fas fa-shopping-cart fa-xs"></i> </a>
+         <ul class="dropdown-menu dropdown-cart" role="menu">
+             <li>
+                 <span class="item">
+                   <span class="item-left">
+                       <img src="http://lorempixel.com/50/50/" alt="" />
+                       <span class="item-info">
+                           <span>Item name</span>
+                           <span>23$</span>
+                       </span>
+                   </span>
+                   <span class="item-right">
+                       <button class="btn btn-xs btn-danger pull-right">x</button>
+                   </span>
+               </span>
+             </li>
+             <li>
+                 <span class="item">
+                   <span class="item-left">
+                       <img src="http://lorempixel.com/50/50/" alt="" />
+                       <span class="item-info">
+                           <span>Item name</span>
+                           <span>23$</span>
+                       </span>
+                   </span>
+                   <span class="item-right">
+                       <button class="btn btn-xs btn-danger pull-right">x</button>
+                   </span>
+               </span>
+             </li>
+             <li>
+                 <span class="item">
+                   <span class="item-left">
+                       <img src="http://lorempixel.com/50/50/" alt="" />
+                       <span class="item-info">
+                           <span>Item name</span>
+                           <span>23$</span>
+                       </span>
+                   </span>
+                   <span class="item-right">
+                       <button class="btn btn-xs btn-danger pull-right">x</button>
+                   </span>
+               </span>
+             </li>
+             <li>
+                 <span class="item">
+                   <span class="item-left">
+                       <img src="http://lorempixel.com/50/50/" alt="" />
+                       <span class="item-info">
+                           <span>Item name</span>
+                           <span>23$</span>
+                       </span>
+                   </span>
+                   <span class="item-right">
+                       <button class="btn btn-xs btn-danger pull-right">x</button>
+                   </span>
+               </span>
+             </li>
+             <li class="divider"></li>
+             	<button type="button" class="btn btn-success">Success</button>
+         </ul>
+       </li>
+          </div>
         </div>
       </div>
+
     </nav>
+
 </template>
 <script>
-export default {
 
-  data () {
-    name: "LandingHeader"
-    return {
-      
-    }
-  }
-}
+export default {
+  data() {
+    name: 'LandingHeader';
+    return {};
+  },
+};
+
 </script>
 
-<style lang="css">
-@import '../custom_css/landing.scss'
+<style scoped lang="scss">
+@import '../custom_css/landing.scss';
 </style>
