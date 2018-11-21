33eb69e6fefb991854e8464a7ca20722d99a2103
Dave Safanyuk <dsafanyuk@gmail.com>
2018-11-20 19:07:16 -0600

    Product filter (#31)
    * Added filter function
    
    * Updated category tabs + implemented product filtering
    
    * Added dollar sign and rounded to 2 decimal places

--- client/src/Components/Landing.vue
+++ client/src/Components/Landing.vue
@@ -1,32 +1,36 @@
 <template>
-<v-app>
-    <LandingHeader> </LandingHeader>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <ul class="  nav justify-content-center">
-                    <li class="cat_menu nav-item">
-                        <a class="nav-link" href="#">Food</a>
-                    </li>
-                    <li class="cat_menu nav-item">
-                        <a class="nav-link" href="#">Snacks</a>
-                    </li>
-                    <li class="cat_menu nav-item">
-                        <a class="nav-link" href="#">Drink</a>
-                    </li>
-                    <li class="cat_menu nav-item">
-                        <a class="nav-link" href="#">  </a>
-                    </li>
-                </ul>
+    <v-app>
+        <LandingHeader></LandingHeader>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="category-wrapper">
+                        <input v-model="selectedCategory" id="tab1" type="radio" name="tabs" value="Popular">
+                        <label for="tab1">
+                            <span>Popular
+                            <i class="material-icons" style="vertical-align: middle;">whatshot</i>
+                            </span>
+                        </label>
+                        <input v-model="selectedCategory" id="tab2" type="radio" name="tabs" value="Snacks">
+                        <label for="tab2">Snacks</label>
+                        <input v-model="selectedCategory" id="tab3" type="radio" name="tabs" value="Drinks">
+                        <label for="tab3">Drinks</label>
+                        <input v-model="selectedCategory" id="tab4" type="radio" name="tabs" value="School Supplies">
+                        <label for="tab4">School Supplies</label>
+                        <input v-model="selectedCategory" id="tab5" type="radio" name="tabs" value="Misc">
+                        <label for="tab5">Misc</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <LandingCard
+                    v-for="product in filteredProducts"
+                    :key="product.product_id"
+                    :product="product"
+                ></LandingCard>
             </div>
         </div>
-        <div class="row">
-                <LandingCard v-for="product in products" :key="product.product_id" :product=product ></LandingCard>
-        </div>
-        </div>
-        <LandingFooter> </LandingFooter>
-
-
+        <LandingFooter></LandingFooter>
     </v-app>
 </template>
 
@@ -42,24 +46,37 @@ export default {
   data() {
     return {
       products: {},
+      selectedCategory: 'Popular',
     };
   },
   props: {},
-  mounted: function loadProducts() {
+  created: function loadProducts() {
     let loadingProductsToast = this.$toasted.show('Loading products...');
     axios
       .get('http://localhost:3000/api/products')
       .then((response) => {
         this.products = response.data;
-        console.log(this.products);
         loadingProductsToast.text('Products loaded!').goAway(5000);
       })
       .catch((error) => {
-        console.log(error);
+          console.log(error);
         loadingProductsToast.goAway();
         this.$toasted.error('Something went wrong');
       });
   },
+  computed: {
+    filteredProducts() {
+        var category = this.selectedCategory.toLowerCase().split(' ').join('_')
+        console.log(`Category = ${category}`)
+        if (category === 'popular' || !category){
+            return this.products;
+        } else {
+            return this.products.filter((product) => {
+                return product.category === category;
+            });
+        }
+    },
+},
   components: {
     LandingHeader: LandingHeader,
     LandingFooter: LandingFooter,
