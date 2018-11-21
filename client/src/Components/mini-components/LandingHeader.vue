e32d84ff4e86f6f39d947589d57f4686f881c3e3
jjohnson247 <43153030+jjohnson247@users.noreply.github.com>
2018-11-20 18:45:17 -0600

    Update LandingHeader.vue (#33)

--- client/src/Components/mini-components/LandingHeader.vue
+++ client/src/Components/mini-components/LandingHeader.vue
@@ -1,100 +1,62 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-        <ul class="custom_url col-md-4 navbar-nav  mt-2 ">
-          <li class="nav-item active">
-            <img src="https://s3.us-east-2.amazonaws.com/fetchrapp/Logo.png" width="80" height="80" class="d-inline-block align-top"align="left">
-          </li>
-        </ul>
-        <form class="col-md-4 form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2 " size="40" type="search" placeholder="Search" aria-label="Search">
-        </form>
-        <div class="col-md-4 icons_div">
-          <div class="col-md-3">
-          </div>
-          <div class="col-md-4" style="display: -webkit-inline-box;">
-          <i class="far fa-user fa-xs"></i>
-       <li class="dropdown">
-         <a href="#" class="" data-toggle="dropdown" role="button" aria-expanded="false"> <i class="fas fa-shopping-cart fa-xs"></i> </a>
-         <ul class="dropdown-menu dropdown-cart" role="menu">
-             <li>
-                 <span class="item">
-                   <span class="item-left">
-                       <img src="http://lorempixel.com/50/50/" alt="" />
-                       <span class="item-info">
-                           <span>Item name</span>
-                           <span>23$</span>
-                       </span>
-                   </span>
-                   <span class="item-right">
-                       <button class="btn btn-xs btn-danger pull-right">x</button>
-                   </span>
-               </span>
-             </li>
-             <li>
-                 <span class="item">
-                   <span class="item-left">
-                       <img src="http://lorempixel.com/50/50/" alt="" />
-                       <span class="item-info">
-                           <span>Item name</span>
-                           <span>23$</span>
-                       </span>
-                   </span>
-                   <span class="item-right">
-                       <button class="btn btn-xs btn-danger pull-right">x</button>
-                   </span>
-               </span>
-             </li>
-             <li>
-                 <span class="item">
-                   <span class="item-left">
-                       <img src="http://lorempixel.com/50/50/" alt="" />
-                       <span class="item-info">
-                           <span>Item name</span>
-                           <span>23$</span>
-                       </span>
-                   </span>
-                   <span class="item-right">
-                       <button class="btn btn-xs btn-danger pull-right">x</button>
-                   </span>
-               </span>
-             </li>
-             <li>
-                 <span class="item">
-                   <span class="item-left">
-                       <img src="http://lorempixel.com/50/50/" alt="" />
-                       <span class="item-info">
-                           <span>Item name</span>
-                           <span>23$</span>
-                       </span>
-                   </span>
-                   <span class="item-right">
-                       <button class="btn btn-xs btn-danger pull-right">x</button>
-                   </span>
-               </span>
-             </li>
-             <li class="divider"></li>
-             	<button type="button" class="btn btn-success">Success</button>
-         </ul>
-       </li>
+      <!-- Image and text -->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul class="custom_url col-md-4 navbar-nav  mt-2 ">
+            <li class="nav-item active">
+              <img src="/lil_logo.png" width="80" height="80" class="d-inline-block align-top"align="left">
+            </li>
+          </ul>
+          <form class="col-md-4 form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2 " size="40" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
+          </form>
+          <div class="col-md-4 ">
+            <div class=" icons_div btn-group " style=" float: right;">
+             <button type="button" class="btn btn-secondary">Hi Josh!</button>
+             <button type="button" class="btn btn-secondary btn_space dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+             <span class="sr-only">Toggle Dropdown</span>
+            </button>
+               <div class="dropdown-menu">
+               <a class="dropdown-item" href="#">My Account</a>
+               <a class="dropdown-item" href="#">My Orders</a>
+               <div class="dropdown-divider"></div>
+               <a class="dropdown-item" href="#">Log Out</a>
+               </div>
+            <i class="fas fa-shopping-cart fa-2x"></i>
           </div>
         </div>
-      </div>
-
-    </nav>
+        </div>
+      </nav>
 
 </template>
 <script>
-
 export default {
-  data() {
-    name: 'LandingHeader';
-    return {};
-  },
-};
 
+  data () {
+    name: "LandingHeader"
+    return {
+
+    }
+  }
+}
 </script>
 
-<style scoped lang="scss">
-@import '../custom_css/landing.scss';
+<style lang="css">
+@import '../custom_css/landing.scss'
+@import '..https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
+@import '..https://use.fontawesome.com/releases/v5.4.1/css/all.css'
+@import '..https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+@import '..https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'
+@import '..https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
+
+.icons_div {
+float : right ;
+}
+
+.btn_space {
+  margin-right: 10px;
+}
+
+
 </style>
