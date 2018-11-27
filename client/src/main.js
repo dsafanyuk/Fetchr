fd64f34c937fd9783ab734ea6d625a7cf839ece2
calebmurrays <caleb.murray@students.pcci.edu>
2018-11-26 19:21:46 -0600

    Recent orders (#36)
    * recent orders page
    
    * fix date method
    
    * order summary
    
    * card
    
    * fixed icons
    
    * Update apiary.apib
    
    * Rename RecentOrders.vue to Orders.vue
    
    * Update main.js
    
    * Update Orders.vue
    
    * Create orders.css
    
    * Update Orders.vue
    
    * Update Orders.vue

--- client/src/main.js
+++ client/src/main.js
@@ -4,10 +4,11 @@ import VueToast from 'vue-toasted'
 
 import App from './Components/App.vue'
 import VueRouter from 'vue-router'
-import Home from './Components/Home.vue'
-import Login from './Components/Login.vue'
-import Register from './Components/Register.vue'
-import Landing from './Components/Landing.vue'
+import Home      from './Components/Home.vue'
+import Login     from './Components/Login.vue'
+import Register  from './Components/Register.vue'
+import Landing   from './Components/Landing.vue'
+import Orders    from './Components/Orders.vue'
 
 import VueMaterial from 'vue-material'
 import 'vuetify/dist/vuetify.min.css'
@@ -18,10 +19,11 @@ Vue.use(VueToast)
 Vue.use(VueMaterial)
 /*----------------------- Routes Declaration -----------------*/
 const routes = [
-  { path: '/', component: Home },
-  { path: '/login', component: Login },
-  { path: '/register', component: Register },
-  { path: '/dashboard', component: Landing }
+  {path : '/',         component : Home      },
+  {path : '/login',    component : Login     },
+  {path : '/register', component : Register  },
+  {path : '/dashboard',  component : Landing },
+  {path : '/orders',  component : Orders }
 ];
 
 const router = new VueRouter({
