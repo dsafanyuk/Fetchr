a0a7d48ceb467b80a640632d4736d7d62dc1f779
Dave Safanyuk <dsafanyuk@gmail.com>
2019-03-25 15:03:56 -0500

    Mobile optimize (#90)
    * Edited View Order to be more mobile-friendly
    
    * Made the checkout a little more mobile friendly
    
    * navigating to a new route should now bring you to the top of the page
    
    * Fixed checkout to be more mobile-friendly
    
    * Mobile Improvements + Removing unused css
    
    * merge

--- client/src/Components/layouts/MainDashboard/components/Confirmation.vue
+++ client/src/Components/layouts/MainDashboard/components/Confirmation.vue
@@ -1,33 +1,33 @@
 <template>
-  <v-app>
-    <div class="confirmation">
-      <h3 class="thankyou">Thank you for using our service!</h3>
-      <div class="userInfo">
-        <p>Your order number is {{this.$route.query.order}}.</p>
-        <p>You can check the status of your orders in the My Orders tab.</p><br>
-        <p>Redirecting to your order page in... {{countdownTime}}</p><br>
-        <p> OR </p><br>
-        <v-btn type="submit" v-on:click="goToView" color="accent" class="white--text">Click here now!</v-btn>
-      </div>
+  <v-container fluid>
+    <h3 class="thankyou">Thank you for using our service!</h3>
+    <div class="userInfo">
+      <p>Your order number is {{this.$route.query.order}}.</p>
+      <p>You can check the status of your orders in the My Orders tab.</p>
+      <br>
+      <p>Redirecting to your order page in... {{countdownTime}}</p>
+      <br>
+      <p>OR</p>
+      <br>
+      <v-btn type="submit" v-on:click="goToView" color="accent" class="white--text">Click here now!</v-btn>
     </div>
-  </v-app>
+  </v-container>
 </template>
 
 
 <script>
-
 export default {
   data() {
     return {
       countdownTime: 10,
       countdownInterval: null,
-      countdownTimeout: null,
+      countdownTimeout: null
     };
   },
   created: function emptyCart() {
     this.$store.getters["cart/cartItems"].map(product => {
       this.$store.commit("cart/removeItem", product);
-    })
+    });
 
     this.countdownInterval = setInterval(() => {
       this.countdownTime--;
@@ -36,20 +36,32 @@ export default {
     this.countdownTimeout = setTimeout(() => {
       clearInterval(this.countdownInterval);
 
-      this.$router.push('/view?order=' + this.$route.query.order);
+      this.$router.push("/view?order=" + this.$route.query.order);
     }, 10000);
   },
   methods: {
     goToView() {
-      this.$router.push('/view?order=' + this.$route.query.order);
+      this.$router.push("/view?order=" + this.$route.query.order);
     }
   },
-  beforeDestroy: function () {
+  beforeDestroy: function() {
     clearInterval(this.countdownInterval);
     clearTimeout(this.countdownTimeout);
   }
 };
 </script>
         
-<style scoped lang="css" src='../../../custom_css/confirmation.css'>
+<style scoped lang="css" >
+.thankyou {
+  margin-bottom: 1em;
+  text-align: center;
+}
+
+.userInfo {
+  text-align: center;
+}
+
+.userInfo p {
+  margin-bottom: 0.5em !important;
+}
 </style>
