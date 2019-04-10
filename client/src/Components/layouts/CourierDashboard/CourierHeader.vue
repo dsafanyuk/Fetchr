a19f8233898ce34fb08168736b7b18ece3c841e4
Dave Safanyuk <dsafanyuk@gmail.com>
2019-04-10 10:42:27 -0500

    added link to logo (#145)

--- client/src/Components/layouts/CourierDashboard/CourierHeader.vue
+++ client/src/Components/layouts/CourierDashboard/CourierHeader.vue
@@ -22,21 +22,22 @@
       <v-toolbar dark color="primary">
         <v-toolbar-side-icon class="hidden-lg-and-up" @click.stop="drawer = !drawer" large></v-toolbar-side-icon>
         <v-spacer class="hidden-lg-and-up"></v-spacer>
-        <img
-          :src="require('../../images/fetchr_header_logo.png')"
-          style="height:65px; width:215px;"
-        >
+        <router-link to="/dashboard">
+          <img
+            :src="require('../../images/fetchr_header_logo.png')"
+            style="height:65px; width:215px;"
+          >
+        </router-link>
+
         <v-spacer></v-spacer>
         <v-flex align-self-center style="margin-right:10px" class="hidden-sm-and-down">
-            <transition name="fade" v-on:enter="enter" v-on:leave="leave">
-                <h4 class="white--text" style="margin-top: 10px" v-if="show">{{showText}}</h4>
-            </transition>
+          <transition name="fade" v-on:enter="enter" v-on:leave="leave">
+            <h4 class="white--text" style="margin-top: 10px" v-if="show">{{showText}}</h4>
+          </transition>
         </v-flex>
         <div class="hidden-md-and-down">
           <v-menu fixed bottom left>
-            <v-btn flat slot="activator" color="white" light>
-              Hi {{firstName}} &nbsp;&nbsp;&nbsp;▼
-            </v-btn>
+            <v-btn flat slot="activator" color="white" light>Hi {{firstName}} &nbsp;&nbsp;&nbsp;▼</v-btn>
             <v-list dense class="pt-0" style="cursor: pointer">
               <v-list-tile
                 v-for="menuItem in menu"
@@ -57,53 +58,50 @@
           <v-dialog v-model="helpDialog" width="800">
             <template v-slot:activator="{ on }">
               <v-btn depressed icon v-on="on">
-                <v-icon color="white">help </v-icon>
+                <v-icon color="white">help</v-icon>
               </v-btn>
             </template>
             <v-card>
               <v-stepper v-model="help" non-linear>
                 <v-stepper-header>
-                  <v-stepper-step v-on:click="help = 1" editable step="1">
-                    Accept an order
-                  </v-stepper-step>
+                  <v-stepper-step v-on:click="help = 1" editable step="1">Accept an order</v-stepper-step>
                   <v-divider></v-divider>
-                  <v-stepper-step v-on:click="help = 2" editable step="2">
-                    Deliver an order
-                  </v-stepper-step>
+                  <v-stepper-step v-on:click="help = 2" editable step="2">Deliver an order</v-stepper-step>
                   <v-divider></v-divider>
-                  <v-stepper-step v-on:click="help = 3" editable step="3">
-                    View order
-                  </v-stepper-step>
+                  <v-stepper-step v-on:click="help = 3" editable step="3">View order</v-stepper-step>
                 </v-stepper-header>
                 <v-stepper-items>
-                  <v-stepper-content
-                    :step="help">
-                    <v-card class="mb-5" color="grey lighten-1">
-                      <v-img :src="gifs[help-1]"></v-img>
-                    </v-card>
-                    <div class="text-xs-center">
-                      {{ instructions[help-1] }}
-                    </div>
-                    <br>
-                    <div class="text-xs-center">
-                      <v-btn 
-                        class="text-xs-center"
-                        color="primary"
-                        :disabled="help == 1"
-                        v-on:click="help--">
-                        back
-                      </v-btn>
-                      <v-btn v-on:click="helpDialog = !helpDialog" class="text-xs-center" color="primary">
-                        close
-                      </v-btn>
-                      <v-btn 
-                        class="text-xs-center"
-                        color="primary"
-                        :disabled="help == 3"
-                        v-on:click="help++">
-                        next
-                      </v-btn>
-                    </div>
+                  <v-stepper-content :step="help">
+                    <v-layout row wrap align-center justify-center fill-height>
+                      <v-flex xs12>
+                        <v-card class="mb-5" color="grey lighten-1">
+                          <v-img :src="gifs[help-1]"></v-img>
+                        </v-card>
+                      </v-flex>
+                      <v-flex xs10 offset-xs-1 class="text-xs-center">
+                        <span class="text-xs-center">{{ instructions[help-1] }}</span>
+                        <br>
+                      </v-flex>
+                      <v-flex class="text-xs-center">
+                        <v-btn
+                          class="text-xs-center"
+                          color="primary"
+                          :disabled="help == 1"
+                          v-on:click="help--"
+                        >back</v-btn>
+                        <v-btn
+                          v-on:click="helpDialog = !helpDialog"
+                          class="text-xs-center"
+                          color="primary"
+                        >close</v-btn>
+                        <v-btn
+                          class="text-xs-center"
+                          color="primary"
+                          :disabled="help == 3"
+                          v-on:click="help++"
+                        >next</v-btn>
+                      </v-flex>
+                    </v-layout>
                   </v-stepper-content>
                 </v-stepper-items>
               </v-stepper>
@@ -141,25 +139,21 @@ export default {
       helpDialog: false,
       help: 1,
       instructions: [
-        "Accept an order from available orders",
-        "Go to 'accepted' tab, deliver the order",
-        "Once it's delivered, you can click the row for further details",
+        "You can view all orders available for delivery in the 'AVAILABLE' tab. Click an order to accept it.",
+        "In the 'ACCEPTED' tab you can click the order to mark it as delivered or chat with a customer.",
+        "You can view all your previous orders in the 'DELIVERED' tab. Click to view order details."
       ],
-      gifs: [
-        CourierAcceptOrder,
-        CourierDeliverOrder,
-        CourierViewOrder,
-      ],
+      gifs: [CourierAcceptOrder, CourierDeliverOrder, CourierViewOrder],
       textLists: [
         "Remember, you cannot change your password",
-        "You can go to shopping page by clicking Fetchr icon",
-        "All items are non refundable",
-        "Try favoriting an item",
+        "You can go to shopping page by clicking Fetchr logo",
+        "All items are non-refundable sorry!",
+        "Chat with a customer if you have a problem"
       ],
       showText: "",
       textTimeout: null,
       show: false,
-      indexText: 0,
+      indexText: 0
     };
   },
   created: function() {
@@ -167,12 +161,12 @@ export default {
     this.showText = this.textLists[this.randomIndex()];
     this.textTimeout = setTimeout(() => {
       this.show = true;
-    },3000);
+    }, 3000);
   },
   watch: {
     // If dialog is closed, instructions go back to step 1
     helpDialog: function(value) {
-      if(!value) {
+      if (!value) {
         this.help = 1;
       }
     }
@@ -192,8 +186,8 @@ export default {
         this.show = true;
       }, 3000);
     },
-    randomIndex: function () {
-      return Math.floor(Math.random() * this.textLists.length)
+    randomIndex: function() {
+      return Math.floor(Math.random() * this.textLists.length);
     },
     showWallet: function(value) {
       this.$store.commit("wallet/toggleWallet", value);
@@ -220,8 +214,8 @@ export default {
                 for (let cookieName in allCookies) {
                   browserCookies.erase(cookieName);
                 }
-                
-                window.localStorage.removeItem('vuex');
+
+                window.localStorage.removeItem("vuex");
                 this.$router.push("/login");
               },
               error => {
@@ -231,7 +225,7 @@ export default {
           }
           break;
       }
-    },
+    }
   }
 };
 </script>
