c25702970c716758c6fb579d14d897700391eac7
jameslim0197 <43100666+jameslim0197@users.noreply.github.com>
2019-04-09 23:02:08 -0500

    Home fix (#130)
    * now user can go to home while logged in
    
    * notification fix and redirect on error 403

--- client/src/router.js
+++ client/src/router.js
@@ -87,8 +87,11 @@ const routes = [
           next();
         }
         else {
-          next({ path: '/login' });
+          next({ path: '/dashboard' });
         }
+      })
+      .catch(error => {
+        next({ path: '/dashboard' })
       });
     },
     children: [
@@ -165,7 +168,6 @@ const routes = [
   {
     path: '/home',
     component: Home,
-    beforeEnter: requireLoggedOut,
   },
   { 
     path: '/login',
